95ecf823a97e5b919242f9e1fd58620586c53a4f
cpandya25 <c.pandya.ext@cleverpush.com>
2026-07-14 09:45:52 +0530

    update Package.swift for version 1.2.3
    update Package.swift for version 1.2.3

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EcondaTracking_Swift",
-            url: "https://github.com/dymatrixgmbh/tracking-ios/releases/download/1.2.2/EcondaTracking_Swift.xcframework.zip",
+            url: "https://github.com/dymatrixgmbh/tracking-ios/releases/download/1.2.3/EcondaTracking_Swift.xcframework.zip",
             checksum: "d789d047beb8a239ea8ee401de503165d3bf0b7b80844b9984213d8f9fd7402e"
         )
     ]
